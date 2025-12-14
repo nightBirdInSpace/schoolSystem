d1f3b6bf38adac64d0dce47ffd2f33287be3cc86
anisa <anizau33@gmail.com>
2025-12-13 23:36:37 -0500

    initialize method isPostalCodeValid(Sting postalCode)

--- src/main/java/org/anisa/Address.java
+++ src/main/java/org/anisa/Address.java
@@ -7,4 +7,24 @@ public class Address {
     private Province province;
     private String postalCode;
 
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            char c = postalCode.charAt(i);
+            if (i % 2 == 0) {
+                if (!Character.isLetter(c)) {
+                    return false;
+                    }
+            } else {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+        }
+
+    return true;
+    }
 }
