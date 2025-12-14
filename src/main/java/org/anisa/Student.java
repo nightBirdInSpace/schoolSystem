f3ce622d8b46c02118657d049bd86d1b30c85313
anisa <anizau33@gmail.com>
2025-12-14 18:03:55 -0500

    initialize constructor and dropCourse methods

--- src/main/java/org/anisa/Student.java
+++ src/main/java/org/anisa/Student.java
@@ -1,5 +1,7 @@
 package org.anisa;
 
+import java.util.ArrayList;
+
 public class Student {
     private String studentId;
     private String studentName;
@@ -8,6 +10,15 @@ public class Student {
     private Department department;
     private ArrayList<Course> registeredCourses;
     private static int nextId = 1;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = Util.toTitleCase(studentName);
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<>();
+    }
 
     public boolean registerCourse (Course course) {
         if (couse == null || registeredCourses.contains(course)) {
@@ -19,5 +30,19 @@ public class Student {
         course.registeredStudent(this);
 
         return true;
+    }
+
+    public boolean dropCourse(Course course) {
+        if (course == null || !registeredCourses.contains(course)) {
+            return false;
+        }
+
+        registeredCourses.remove(course);
+
+        int studentIndex = course.getRegisteredStudents().indexOf(this);
+
+        if (studentIndex != -1) {
+            course.getRegisteredStudents().remove(studentIndex);
+        }
     }
 }
