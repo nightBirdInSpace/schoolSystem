510609878d555df985ceb9bb6c731bd009ee90da
anisa <anizau33@gmail.com>
2025-12-14 01:08:44 -0500

    initialize constructor for the Address class

--- src/main/java/org/anisa/Address.java
+++ src/main/java/org/anisa/Address.java
@@ -27,4 +27,20 @@ public class Address {
 
     return true;
     }
+
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
+    }
 }
